a41c5e6f62ca406d4e9703bb00696cbe28350cb2
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2024-08-14 20:49:09 -0300

    Update DBRepository.java

--- src/main/java/org/example/database/DBRepository.java
+++ src/main/java/org/example/database/DBRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public interface DBRepository<T, ID> {
 
+    T save(T entity);
+
     T findById(ID id);
 
     List<T> findAll();
-
-    T save(T entity);
 
     Integer delete(ID id);
 }
