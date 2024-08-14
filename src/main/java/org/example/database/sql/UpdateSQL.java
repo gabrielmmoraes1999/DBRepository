84a2fc90b866c44e70f3089c41369da3668a3b1e
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2024-08-14 20:13:16 -0300

    Update UpdateSQL.java

--- src/main/java/org/example/database/sql/UpdateSQL.java
+++ src/main/java/org/example/database/sql/UpdateSQL.java
@@ -5,6 +5,8 @@ import org.example.database.annotation.PrimaryKey;
 import org.example.database.annotation.Table;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UpdateSQL {
 
@@ -21,29 +23,18 @@ public class UpdateSQL {
         StringBuilder sqlBuilder = new StringBuilder("UPDATE ");
         sqlBuilder.append(table.name()).append(" SET ");
 
-        Field primaryKeyField = null;
+        List<Field> fieldList = new ArrayList<>();
         StringBuilder setClause = new StringBuilder();
-        Object idValue = null;
 
         for (Field field : entityClass.getDeclaredFields()) {
             if (field.isAnnotationPresent(PrimaryKey.class)) {
-                primaryKeyField = field;
+                fieldList.add(field);
+                continue;
             }
+
             if (field.isAnnotationPresent(Column.class)) {
                 Column column = field.getAnnotation(Column.class);
-                field.setAccessible(true);
-                Object value = field.get(entity);
-
-                if (primaryKeyField != null && primaryKeyField.equals(field)) {
-                    idValue = value;
-                } else {
-                    setClause.append(column.name()).append(" = ");
-                    if (value instanceof String || value instanceof java.sql.Date) {
-                        setClause.append("'").append(value).append("', ");
-                    } else {
-                        setClause.append(value).append(", ");
-                    }
-                }
+                setClause.append(column.name()).append(" = ?, ");
             }
         }
 
@@ -54,16 +45,16 @@ public class UpdateSQL {
 
         sqlBuilder.append(setClause).append(" WHERE ");
 
-        assert primaryKeyField != null;
-        Column primaryKeyColumn = primaryKeyField.getAnnotation(Column.class);
-        sqlBuilder.append(primaryKeyColumn.name()).append(" = ");
+        fieldList.forEach(primaryKey -> {
+            Column primaryKeyColumn = primaryKey.getAnnotation(Column.class);
+            sqlBuilder.append(primaryKeyColumn.name()).append(" = ? AND ");
+        });
 
-        if (idValue instanceof String || idValue instanceof java.sql.Date) {
-            sqlBuilder.append("'").append(idValue).append("'");
-        } else {
-            sqlBuilder.append(idValue);
+        if (sqlBuilder.length() > 0) {
+            sqlBuilder.setLength(sqlBuilder.length() - 5);
         }
 
+        sqlBuilder.append(";");
         return sqlBuilder.toString();
     }
 }
