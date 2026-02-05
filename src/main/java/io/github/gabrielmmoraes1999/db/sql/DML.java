94ea1148400a653b0d59d932aa289617697b135c
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2026-02-05 19:19:51 -0300

    Update DML.java

--- src/main/java/io/github/gabrielmmoraes1999/db/sql/DML.java
+++ src/main/java/io/github/gabrielmmoraes1999/db/sql/DML.java
@@ -1,9 +1,10 @@
 package io.github.gabrielmmoraes1999.db.sql;
 
-import io.github.gabrielmmoraes1999.db.annotation.Column;
-import io.github.gabrielmmoraes1999.db.annotation.Table;
+import io.github.gabrielmmoraes1999.db.annotation.*;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
 import java.sql.*;
 import java.sql.Date;
@@ -11,7 +12,7 @@ import java.util.*;
 
 public class DML {
 
-    public static void insert(Object entity, Connection connection) throws SQLException {
+    public static int insert(Object entity, Connection connection) throws SQLException, IllegalAccessException {
         Class<?> clazz = entity.getClass();
 
         if (!clazz.isAnnotationPresent(Table.class)) {
@@ -33,18 +34,219 @@ public class DML {
             Column column = field.getAnnotation(Column.class);
             field.setAccessible(true);
 
-            try {
-                if (Objects.nonNull(field.get(entity))) {
-                    columns.add(column.name());
-                    values.add("?");
-                    fields.add(field);
-                }
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
+            if (Objects.nonNull(field.get(entity))) {
+                columns.add(column.name());
+                values.add("?");
+                fields.add(field);
             }
         }
 
         String sql = String.format("INSERT INTO %s (%s) VALUES (%s)", tableName, columns, values);
+        return DML.prepareStatement(sql, entity, fields, connection);
+    }
+
+    public static int insertCascade(Object entity, Connection connection) throws SQLException, IllegalAccessException {
+        Class<?> clazz = entity.getClass();
+        int result = insert(entity, connection);
+
+        for (Field field : clazz.getDeclaredFields()) {
+            if (!field.isAnnotationPresent(JoinColumns.class)) {
+                continue;
+            }
+
+            if (field.isAnnotationPresent(OneToMany.class)) {
+                field.setAccessible(true);
+                Object value = field.get(entity);
+
+                if (value == null) {
+                    continue;
+                }
+
+                if (!(value instanceof Collection)) {
+                    continue;
+                }
+
+                for (Object child : (Collection<?>) value) {
+                    copyJoinColumns(entity, child, field);
+                    result = result + insert(child, connection);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void update(Object entity, Connection connection) throws SQLException, IllegalAccessException {
+        Class<?> clazz = entity.getClass();
+
+        if (!clazz.isAnnotationPresent(Table.class)) {
+            throw new IllegalArgumentException("Classe sem @Table");
+        }
+
+        Table table = clazz.getAnnotation(Table.class);
+        String tableName = table.name();
+
+        StringJoiner setClause = new StringJoiner(", ");
+        StringJoiner whereClause = new StringJoiner(", ");
+        List<Field> updateFields = new ArrayList<>();
+        List<Field> primaryKeyFields = new ArrayList<>();
+        List<Field> fields = new ArrayList<>();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            if (!field.isAnnotationPresent(Column.class)) {
+                continue;
+            }
+
+            Column column = field.getAnnotation(Column.class);
+            if (field.isAnnotationPresent(PrimaryKey.class)) {
+                whereClause.add(String.format("%s = ?", column.name()));
+                primaryKeyFields.add(field);
+            } else {
+                setClause.add(String.format("%s = ?", column.name()));
+                updateFields.add(field);
+            }
+        }
+
+        if (primaryKeyFields.isEmpty()) {
+            throw new IllegalArgumentException("Classe sem @PrimaryKey");
+        }
+
+        for (Field field : primaryKeyFields) {
+            field.setAccessible(true);
+
+            if (Objects.isNull(field.get(entity))) {
+                throw new IllegalArgumentException("Valor do @PrimaryKey é nulo");
+            }
+        }
+
+        fields.addAll(updateFields);
+        fields.addAll(primaryKeyFields);
+        String sql = String.format("UPDATE %s SET %s WHERE %s", tableName, setClause, whereClause);
+        DML.prepareStatement(sql, entity, fields, connection);
+    }
+
+    public static void updateCascade(Object entity, Connection connection) throws SQLException, IllegalAccessException {
+        update(entity, connection);
+        Class<?> clazz = entity.getClass();
+
+        // 2. Processa relações @OneToMany
+        for (Field field : clazz.getDeclaredFields()) {
+
+            if (!field.isAnnotationPresent(OneToMany.class)) {
+                continue;
+            }
+
+            field.setAccessible(true);
+            Object value = field.get(entity);
+
+            // 3. Deleta filhos sempre (mesmo se lista for null ou vazia)
+            deleteChildren(entity, field, connection);
+
+            if (value == null) {
+                continue;
+            }
+
+            if (!(value instanceof Collection)) {
+                continue;
+            }
+
+            Collection<?> collection = (Collection<?>) value;
+
+            // 4. Insere novamente os filhos
+            for (Object child : collection) {
+                copyJoinColumns(entity, child, field);
+                insert(child, connection);
+            }
+        }
+    }
+
+    public static void delete(Object entity, Connection connection) throws SQLException, IllegalAccessException {
+        Class<?> clazz = entity.getClass();
+
+        if (!clazz.isAnnotationPresent(Table.class)) {
+            throw new IllegalArgumentException("Classe sem @Table");
+        }
+
+        Table table = clazz.getAnnotation(Table.class);
+        String tableName = table.name();
+
+        StringJoiner whereClause = new StringJoiner(", ");
+        List<Field> fields = new ArrayList<>();
+
+        for (Field field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+
+            if (!field.isAnnotationPresent(Column.class)) {
+                continue;
+            }
+
+            Column column = field.getAnnotation(Column.class);
+            if (field.isAnnotationPresent(PrimaryKey.class)) {
+                whereClause.add(String.format("%s = ?", column.name()));
+                fields.add(field);
+            }
+        }
+
+        if (fields.isEmpty()) {
+            throw new IllegalArgumentException("Classe sem @PrimaryKey");
+        }
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+
+            if (Objects.isNull(field.get(entity))) {
+                throw new IllegalArgumentException("Valor do @PrimaryKey é nulo");
+            }
+        }
+
+        String sql = String.format("UPDATE %s WHERE %s", tableName, whereClause);
+        DML.prepareStatement(sql, entity, fields, connection);
+    }
+
+    private static void deleteChildren(Object parent, Field oneToManyField, Connection connection) throws SQLException, IllegalAccessException {
+
+        JoinColumns joinColumns = oneToManyField.getAnnotation(JoinColumns.class);
+        if (joinColumns == null) {
+            return;
+        }
+
+        // Descobre tabela do filho
+        Class<?> childClass = getCollectionGenericType(oneToManyField);
+        if (!childClass.isAnnotationPresent(Table.class)) {
+            throw new RuntimeException("Classe filha sem @Table");
+        }
+
+        Table table = childClass.getAnnotation(Table.class);
+        String tableName = table.name();
+
+        StringJoiner where = new StringJoiner(" AND ");
+        List<Object> params = new ArrayList<>();
+
+        for (JoinColumn joinColumn : joinColumns.value()) {
+
+            Field parentField = findFieldByColumn(
+                    parent.getClass(),
+                    joinColumn.referencedColumnName());
+
+            if (parentField == null) {
+                throw new RuntimeException(
+                        "Campo não encontrado: " + joinColumn.referencedColumnName());
+            }
+
+            parentField.setAccessible(true);
+
+            where.add(joinColumn.name() + " = ?");
+            params.add(parentField.get(parent));
+        }
+
+        String sql = "DELETE FROM " + tableName + " WHERE " + where;
+
+        DML.execute(sql, params, connection);
+    }
+
+    private static int prepareStatement(String sql, Object entity, List<Field> fields, Connection connection) throws SQLException, IllegalAccessException {
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             for (Field field : fields) {
                 field.setAccessible(true);
@@ -76,10 +278,62 @@ public class DML {
                     preparedStatement.setObject(position, value);
                 }
             }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            return preparedStatement.executeUpdate();
+        }
+    }
+
+    private static Class<?> getCollectionGenericType(Field field) {
+        Type type = field.getGenericType();
+
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            Type[] args = pt.getActualTypeArguments();
+            if (args.length > 0 && args[0] instanceof Class) {
+                return (Class<?>) args[0];
+            }
         }
 
+        throw new RuntimeException(
+                "Não foi possível descobrir tipo genérico de " + field.getName());
+    }
+
+
+    private static void copyJoinColumns(Object parent, Object child, Field oneToManyField) throws IllegalAccessException {
+        JoinColumns joinColumns = oneToManyField.getAnnotation(JoinColumns.class);
+        if (joinColumns == null) return;
+
+        Class<?> parentClass = parent.getClass();
+        Class<?> childClass = child.getClass();
+
+        for (JoinColumn joinColumn : joinColumns.value()) {
+            String referencedColumn = joinColumn.referencedColumnName();
+            Field parentField = findFieldByColumn(parentClass, referencedColumn);
+            Field childField = findFieldByColumn(childClass, joinColumn.name());
+
+            if (parentField == null) {
+                throw new RuntimeException("JoinColumn inválido: '" + joinColumn.name() + "' não existente na tabela pai.");
+            }
+
+            if (childField == null) {
+                throw new RuntimeException("JoinColumn inválido: '" + joinColumn.name() + "' não existente na tabela filha.");
+            }
+
+            parentField.setAccessible(true);
+            childField.setAccessible(true);
+
+            Object value = parentField.get(parent);
+            childField.set(child, value);
+        }
+    }
+
+    private static Field findFieldByColumn(Class<?> clazz, String columnName) {
+        for (Field field : clazz.getDeclaredFields()) {
+            Column column = field.getAnnotation(Column.class);
+            if (column != null && column.name().equals(columnName)) {
+                return field;
+            }
+        }
+        return null;
     }
 
 }
