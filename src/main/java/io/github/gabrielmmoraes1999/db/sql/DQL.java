28771b6df1f49e09a5a0adb72e730c6987b8b244
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2026-02-05 22:31:26 -0300

    Update DQL.java

--- src/main/java/io/github/gabrielmmoraes1999/db/sql/DQL.java
+++ src/main/java/io/github/gabrielmmoraes1999/db/sql/DQL.java
@@ -1,8 +1,6 @@
 package io.github.gabrielmmoraes1999.db.sql;
 
-import io.github.gabrielmmoraes1999.db.annotation.Column;
-import io.github.gabrielmmoraes1999.db.annotation.PrimaryKey;
-import io.github.gabrielmmoraes1999.db.annotation.Table;
+import io.github.gabrielmmoraes1999.db.annotation.*;
 import io.github.gabrielmmoraes1999.db.util.Function;
 
 import java.lang.reflect.Field;
@@ -121,11 +119,197 @@ public class DQL {
                     field.set(entity, rs.getObject(column.name()));
                 }
 
+                DQL.loadOneToOne(entity, connection);
+                DQL.loadOneToMany(entity, connection);
                 return entity;
             }
         }
 
         return null;
+    }
+
+    private static void loadOneToMany(Object parent, Connection connection) throws IllegalAccessException, SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        Class<?> parentClass = parent.getClass();
+
+        for (Field field : parentClass.getDeclaredFields()) {
+            if (!field.isAnnotationPresent(OneToMany.class)) {
+                continue;
+            }
+
+            field.setAccessible(true);
+
+            JoinColumns joinColumns = field.getAnnotation(JoinColumns.class);
+            JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
+
+            List<JoinColumn> joins = new ArrayList<>();
+
+            if (joinColumns != null) {
+                joins.addAll(Arrays.asList(joinColumns.value()));
+            } else if (joinColumn != null) {
+                joins.add(joinColumn);
+            } else {
+                throw new IllegalArgumentException("@OneToOne sem @JoinColumn(s): " + field.getName());
+            }
+
+            Class<?> childClass = SQLUtils.getCollectionGenericType(field);
+
+            if (!childClass.isAnnotationPresent(Table.class)) {
+                throw new IllegalArgumentException("Classe filha sem @Table");
+            }
+
+            Table table = childClass.getAnnotation(Table.class);
+            String tableName = table.name();
+
+            // SELECT colunas
+            StringJoiner select = new StringJoiner(", ");
+            for (Field f : childClass.getDeclaredFields()) {
+                if (f.isAnnotationPresent(Column.class)) {
+                    select.add(f.getAnnotation(Column.class).name());
+                }
+            }
+
+            // WHERE com JoinColumns
+            StringJoiner where = new StringJoiner(" AND ");
+            List<Object> params = new ArrayList<>();
+
+            for (JoinColumn jc : joins) {
+                Field parentField = SQLUtils.findFieldByColumn(parentClass, jc.referencedColumnName());
+
+                if (parentField == null) {
+                    throw new RuntimeException("Campo não encontrado: " + jc.referencedColumnName());
+                }
+
+                parentField.setAccessible(true);
+                Object value = parentField.get(parent);
+
+                where.add(jc.name() + " = ?");
+                params.add(value);
+            }
+
+            List<Object> children = new ArrayList<>();
+            String sql = String.format("SELECT %s FROM %s WHERE %s", select, tableName, where);
+
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                for (int i = 0; i < params.size(); i++) {
+                    ps.setObject(i + 1, params.get(i));
+                }
+
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        Object child = childClass.getDeclaredConstructor().newInstance();
+
+                        for (Field f : childClass.getDeclaredFields()) {
+                            if (!f.isAnnotationPresent(Column.class)) {
+                                continue;
+                            }
+
+                            Column col = f.getAnnotation(Column.class);
+                            f.setAccessible(true);
+                            f.set(child, rs.getObject(col.name()));
+                        }
+
+                        children.add(child);
+                    }
+                }
+            }
+
+            field.set(parent, children);
+        }
+    }
+
+    private static void loadOneToOne(Object parent, Connection connection) throws IllegalAccessException, SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        Class<?> parentClass = parent.getClass();
+
+        for (Field field : parentClass.getDeclaredFields()) {
+            if (!field.isAnnotationPresent(OneToOne.class)) {
+                continue;
+            }
+
+            field.setAccessible(true);
+
+            // Suporta @JoinColumn ou @JoinColumns
+            JoinColumns joinColumns = field.getAnnotation(JoinColumns.class);
+            JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
+
+            List<JoinColumn> joins = new ArrayList<>();
+
+            if (joinColumns != null) {
+                joins.addAll(Arrays.asList(joinColumns.value()));
+            } else if (joinColumn != null) {
+                joins.add(joinColumn);
+            } else {
+                throw new IllegalArgumentException("@OneToOne sem @JoinColumn(s): " + field.getName());
+            }
+
+            Class<?> targetClass = field.getType();
+
+            if (!targetClass.isAnnotationPresent(Table.class)) {
+                throw new IllegalArgumentException("Classe OneToOne sem @Table: " + targetClass.getName());
+            }
+
+            Table table = targetClass.getAnnotation(Table.class);
+            String tableName = table.name();
+
+            // SELECT colunas
+            StringJoiner select = new StringJoiner(", ");
+            for (Field f : targetClass.getDeclaredFields()) {
+                if (f.isAnnotationPresent(Column.class)) {
+                    select.add(f.getAnnotation(Column.class).name());
+                }
+            }
+
+            // WHERE
+            StringJoiner where = new StringJoiner(" AND ");
+            List<Object> params = new ArrayList<>();
+
+            for (JoinColumn jc : joins) {
+                Field parentField = SQLUtils.findFieldByColumn(
+                        parentClass,
+                        jc.referencedColumnName());
+
+                if (parentField == null) {
+                    throw new RuntimeException(
+                            "Campo não encontrado: " + jc.referencedColumnName());
+                }
+
+                parentField.setAccessible(true);
+                Object value = parentField.get(parent);
+
+                where.add(jc.name() + " = ?");
+                params.add(value);
+            }
+
+            String sql = String.format("SELECT %s FROM %s WHERE %s", select, tableName, where);
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+                for (int i = 0; i < params.size(); i++) {
+                    ps.setObject(i + 1, params.get(i));
+                }
+
+                try (ResultSet rs = ps.executeQuery()) {
+
+                    if (rs.next()) {
+                        Object target = targetClass
+                                .getDeclaredConstructor()
+                                .newInstance();
+
+                        for (Field f : targetClass.getDeclaredFields()) {
+                            if (!f.isAnnotationPresent(Column.class)) {
+                                continue;
+                            }
+
+                            Column col = f.getAnnotation(Column.class);
+                            f.setAccessible(true);
+                            f.set(target, rs.getObject(col.name()));
+                        }
+
+                        field.set(parent, target);
+                    } else {
+                        field.set(parent, null);
+                    }
+                }
+            }
+        }
     }
 
 }
