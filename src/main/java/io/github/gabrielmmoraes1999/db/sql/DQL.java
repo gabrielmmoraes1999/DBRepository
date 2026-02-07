018902eda8912008acc86f2e405c53254e379ee1
Gabriel Moraes <gabrielmmoraes1999@icloud.com>
2026-02-06 22:55:30 -0300

    Update DQL.java

--- src/main/java/io/github/gabrielmmoraes1999/db/sql/DQL.java
+++ src/main/java/io/github/gabrielmmoraes1999/db/sql/DQL.java
@@ -24,24 +24,9 @@ public class DQL {
         Table table = Objects.requireNonNull(entityClass.getAnnotation(Table.class));
         String sql = String.format("SELECT * FROM %s", table.name());
 
-        try (Statement statement = connection.createStatement();
-             ResultSet rs = statement.executeQuery(sql)) {
-            while (rs.next()) {
-                T entity = entityClass.getDeclaredConstructor().newInstance();
-
-                for (Field field : entityClass.getDeclaredFields()) {
-                    if (!field.isAnnotationPresent(Column.class)) {
-                        continue;
-                    }
-
-                    Column column = field.getAnnotation(Column.class);
-                    field.setAccessible(true);
-                    field.set(entity, rs.getObject(column.name()));
-                }
-
-                DQL.loadOneToOne(entity, connection);
-                DQL.loadOneToMany(entity, connection);
-                entityList.add(entity);
+        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                entityList.add(Entity.build(entityClass, resultSet, connection));
             }
         }
 
@@ -122,28 +107,14 @@ public class DQL {
 
         String sql = String.format("SELECT %s FROM %s WHERE %s", selectClause, tableName, whereClause);
         try (PreparedStatement preparedStatement = SQLUtils.getPreparedStatement(sql, entity, primaryKeyFields, connection);
-             ResultSet rs = preparedStatement.executeQuery()) {
+             ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            if (rs.next()) {
-                entity = entityClass.getDeclaredConstructor().newInstance();
-
-                for (Field field : entityClass.getDeclaredFields()) {
-                    if (!field.isAnnotationPresent(Column.class)) {
-                        continue;
-                    }
-
-                    Column column = field.getAnnotation(Column.class);
-                    field.setAccessible(true);
-                    field.set(entity, rs.getObject(column.name()));
-                }
-
-                DQL.loadOneToOne(entity, connection);
-                DQL.loadOneToMany(entity, connection);
-                return entity;
+            if (resultSet.next()) {
+                entity = Entity.build(entityClass, resultSet, connection);
             }
         }
 
-        return null;
+        return entity;
     }
 
     public static <T> Object handleMethod(Class<T> entityClass, Method method, Object[] args, Connection connection) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
@@ -174,37 +145,9 @@ public class DQL {
 
                 while (resultSet.next()) {
                     if (returnClass.isAssignableFrom(List.class)) {
-                        T entity = entityClass.getDeclaredConstructor().newInstance();
-
-                        for (Field field : entityClass.getDeclaredFields()) {
-                            if (!field.isAnnotationPresent(Column.class)) {
-                                continue;
-                            }
-
-                            Column column = field.getAnnotation(Column.class);
-                            field.setAccessible(true);
-                            field.set(entity, resultSet.getObject(column.name()));
-                        }
-
-                        DQL.loadOneToOne(entity, connection);
-                        DQL.loadOneToMany(entity, connection);
-                        resultList.add(entity);
+                        resultList.add(Entity.build(entityClass, resultSet, connection));
                     } else if (returnClass.isAssignableFrom(entityClass)) {
-                        T entity = entityClass.getDeclaredConstructor().newInstance();
-
-                        for (Field field : entityClass.getDeclaredFields()) {
-                            if (!field.isAnnotationPresent(Column.class)) {
-                                continue;
-                            }
-
-                            Column column = field.getAnnotation(Column.class);
-                            field.setAccessible(true);
-                            field.set(entity, resultSet.getObject(column.name()));
-                        }
-
-                        DQL.loadOneToOne(entity, connection);
-                        DQL.loadOneToMany(entity, connection);
-                        resultClass = entity;
+                        resultClass = Entity.build(entityClass, resultSet, connection);;
                         break;
                     } else if (returnClass.isAssignableFrom(JSONObject.class)) {
                         for (int i = 1; i <= columnCount; i++) {
@@ -234,190 +177,6 @@ public class DQL {
             return jsonArray;
         } else {
             return null;
-        }
-    }
-
-    protected static void loadOneToMany(Object parent, Connection connection) throws IllegalAccessException, SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException {
-        Class<?> parentClass = parent.getClass();
-
-        for (Field field : parentClass.getDeclaredFields()) {
-            if (!field.isAnnotationPresent(OneToMany.class)) {
-                continue;
-            }
-
-            field.setAccessible(true);
-
-            JoinColumns joinColumns = field.getAnnotation(JoinColumns.class);
-            JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
-
-            List<JoinColumn> joins = new ArrayList<>();
-
-            if (joinColumns != null) {
-                joins.addAll(Arrays.asList(joinColumns.value()));
-            } else if (joinColumn != null) {
-                joins.add(joinColumn);
-            } else {
-                throw new IllegalArgumentException("@OneToOne sem @JoinColumn(s): " + field.getName());
-            }
-
-            Class<?> childClass = SQLUtils.getCollectionGenericType(field);
-
-            if (!childClass.isAnnotationPresent(Table.class)) {
-                throw new IllegalArgumentException("Classe filha sem @Table");
-            }
-
-            Table table = childClass.getAnnotation(Table.class);
-            String tableName = table.name();
-
-            // SELECT colunas
-            StringJoiner select = new StringJoiner(", ");
-            for (Field f : childClass.getDeclaredFields()) {
-                if (f.isAnnotationPresent(Column.class)) {
-                    select.add(f.getAnnotation(Column.class).name());
-                }
-            }
-
-            // WHERE com JoinColumns
-            StringJoiner where = new StringJoiner(" AND ");
-            List<Object> params = new ArrayList<>();
-
-            for (JoinColumn jc : joins) {
-                Field parentField = SQLUtils.findFieldByColumn(parentClass, jc.referencedColumnName());
-
-                if (parentField == null) {
-                    throw new RuntimeException("Campo não encontrado: " + jc.referencedColumnName());
-                }
-
-                parentField.setAccessible(true);
-                Object value = parentField.get(parent);
-
-                where.add(jc.name() + " = ?");
-                params.add(value);
-            }
-
-            List<Object> children = new ArrayList<>();
-            String sql = String.format("SELECT %s FROM %s WHERE %s", select, tableName, where);
-
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
-                for (int i = 0; i < params.size(); i++) {
-                    ps.setObject(i + 1, params.get(i));
-                }
-
-                try (ResultSet rs = ps.executeQuery()) {
-                    while (rs.next()) {
-                        Object child = childClass.getDeclaredConstructor().newInstance();
-
-                        for (Field f : childClass.getDeclaredFields()) {
-                            if (!f.isAnnotationPresent(Column.class)) {
-                                continue;
-                            }
-
-                            Column col = f.getAnnotation(Column.class);
-                            f.setAccessible(true);
-                            f.set(child, rs.getObject(col.name()));
-                        }
-
-                        children.add(child);
-                    }
-                }
-            }
-
-            field.set(parent, children);
-        }
-    }
-
-    protected static void loadOneToOne(Object parent, Connection connection) throws IllegalAccessException, SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException {
-        Class<?> parentClass = parent.getClass();
-
-        for (Field field : parentClass.getDeclaredFields()) {
-            if (!field.isAnnotationPresent(OneToOne.class)) {
-                continue;
-            }
-
-            field.setAccessible(true);
-
-            // Suporta @JoinColumn ou @JoinColumns
-            JoinColumns joinColumns = field.getAnnotation(JoinColumns.class);
-            JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
-
-            List<JoinColumn> joins = new ArrayList<>();
-
-            if (joinColumns != null) {
-                joins.addAll(Arrays.asList(joinColumns.value()));
-            } else if (joinColumn != null) {
-                joins.add(joinColumn);
-            } else {
-                throw new IllegalArgumentException("@OneToOne sem @JoinColumn(s): " + field.getName());
-            }
-
-            Class<?> targetClass = field.getType();
-
-            if (!targetClass.isAnnotationPresent(Table.class)) {
-                throw new IllegalArgumentException("Classe OneToOne sem @Table: " + targetClass.getName());
-            }
-
-            Table table = targetClass.getAnnotation(Table.class);
-            String tableName = table.name();
-
-            // SELECT colunas
-            StringJoiner select = new StringJoiner(", ");
-            for (Field f : targetClass.getDeclaredFields()) {
-                if (f.isAnnotationPresent(Column.class)) {
-                    select.add(f.getAnnotation(Column.class).name());
-                }
-            }
-
-            // WHERE
-            StringJoiner where = new StringJoiner(" AND ");
-            List<Object> params = new ArrayList<>();
-
-            for (JoinColumn jc : joins) {
-                Field parentField = SQLUtils.findFieldByColumn(
-                        parentClass,
-                        jc.referencedColumnName());
-
-                if (parentField == null) {
-                    throw new RuntimeException(
-                            "Campo não encontrado: " + jc.referencedColumnName());
-                }
-
-                parentField.setAccessible(true);
-                Object value = parentField.get(parent);
-
-                where.add(jc.name() + " = ?");
-                params.add(value);
-            }
-
-            String sql = String.format("SELECT %s FROM %s WHERE %s", select, tableName, where);
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
-
-                for (int i = 0; i < params.size(); i++) {
-                    ps.setObject(i + 1, params.get(i));
-                }
-
-                try (ResultSet rs = ps.executeQuery()) {
-
-                    if (rs.next()) {
-                        Object target = targetClass
-                                .getDeclaredConstructor()
-                                .newInstance();
-
-                        for (Field f : targetClass.getDeclaredFields()) {
-                            if (!f.isAnnotationPresent(Column.class)) {
-                                continue;
-                            }
-
-                            Column col = f.getAnnotation(Column.class);
-                            f.setAccessible(true);
-                            f.set(target, rs.getObject(col.name()));
-                        }
-
-                        field.set(parent, target);
-                    } else {
-                        field.set(parent, null);
-                    }
-                }
-            }
         }
     }
 
